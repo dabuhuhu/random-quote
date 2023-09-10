f54788ffd5c80c9e3b28cbe044ed160068f46144
dabuhu <80679542+dabuhuhu@users.noreply.github.com>
2023-09-10 10:50:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const quoteText = document.getElementById("quote-text");
 const newQuoteBtn = document.getElementById("new-quote-btn");
+const copyBtn = document.getElementById("copy-btn"); // Replace shareBtn with copyBtn
 
 newQuoteBtn.addEventListener("click", getNewQuote);
+copyBtn.addEventListener("click", copyQuote); // Replace shareQuote with copyQuote
 
 function getNewQuote() {
     fetch("https://api.quotable.io/random")
@@ -13,6 +15,25 @@ function getNewQuote() {
         .catch((error) => {
             console.error("Error fetching quote:", error);
         });
+}
+
+function copyQuote() {
+    const quote = quoteText.textContent;
+
+    // Create a text area element to hold the quote text
+    const textarea = document.createElement("textarea");
+    textarea.value = quote;
+    document.body.appendChild(textarea);
+
+    // Select and copy the text
+    textarea.select();
+    document.execCommand("copy");
+
+    // Remove the temporary text area element
+    document.body.removeChild(textarea);
+
+    // Provide some feedback to the user
+    alert("Quote copied to clipboard!");
 }
 
 // Initial load
